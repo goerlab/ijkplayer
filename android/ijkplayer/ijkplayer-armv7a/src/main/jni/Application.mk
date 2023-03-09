5d6161b4098c4587448e94ca1d70696da60e0b93
Yonghua Zheng <yonghua.zheng@goertek.com>
2023-03-09 11:07:16 +0800

    Application.mk cleanup
    Use default CFLAGS provided in NDK by default to prevent software
    upgrades and unexpected compiling issues

--- android/ijkplayer/ijkplayer-armv7a/src/main/jni/Application.mk
+++ android/ijkplayer/ijkplayer-armv7a/src/main/jni/Application.mk
@@ -21,12 +21,4 @@ APP_OPTIM := release
 APP_PLATFORM := android-9
 APP_ABI := armeabi-v7a
 NDK_TOOLCHAIN_VERSION=4.9
-APP_PIE := false
-
 APP_STL := c++_static
-
-APP_CFLAGS := -O3 -Wall -pipe \
-    -ffast-math \
-    -fstrict-aliasing -Werror=strict-aliasing \
-    -Wno-psabi -Wa,--noexecstack \
-    -DANDROID -DNDEBUG
--- android/ijkplayer/ijkplayer-x86/src/main/jni/Application.mk
+++ android/ijkplayer/ijkplayer-x86/src/main/jni/Application.mk
@@ -21,12 +21,4 @@ APP_OPTIM := release
 APP_PLATFORM := android-16
 APP_ABI := x86
 NDK_TOOLCHAIN_VERSION=4.9
-APP_PIE := false
-
 APP_STL := c++_static
-
-APP_CFLAGS := -O3 -Wall -pipe \
-    -ffast-math \
-    -fstrict-aliasing -Werror=strict-aliasing \
-    -Wno-psabi -Wa,--noexecstack \
-    -DANDROID -DNDEBUG
